3babb19d670dceb15a9911b68b41c790a5ac1fe0
LangoroMangoro <85540807+LangoroMangoro@users.noreply.github.com>
2022-04-13 15:35:33 +0300

    breadcrumb added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoanCreateComponent } from './features/loan/loan-create/loan-create.component';
 import { LoanListComponent } from './features/loan/loan-list/loan-list.component';
 import { PageLayoutComponent } from './pages/page-layout/page-layout.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoanCreateComponent,
     LoanListComponent,
-    PageLayoutComponent
+    PageLayoutComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
