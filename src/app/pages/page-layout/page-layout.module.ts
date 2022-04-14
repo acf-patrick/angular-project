2372a1b54e733c93be4d1f2ebfc9c8c4d6c5c281
acf-patrick <miharisoap@gmail.com>
2022-04-14 11:53:29 +0300

    sidemenu > dev

--- src/app/pages/page-layout/page-layout.module.ts
+++ src/app/pages/page-layout/page-layout.module.ts
@@ -5,9 +5,10 @@ import { PageLayoutRoutingModule } from './page-layout-routing.module';
 import { PageLayoutComponent } from './page-layout.component';
 import { HeaderComponent } from '../../features/header/header.component';
 import { LoanModule } from 'src/app/features/loan/loan.module';
+import { SidemenuComponent } from 'src/app/features/sidemenu/sidemenu.component';
 
 @NgModule({
-  declarations: [PageLayoutComponent, HeaderComponent],
+  declarations: [PageLayoutComponent, HeaderComponent, SidemenuComponent],
   imports: [CommonModule, PageLayoutRoutingModule, LoanModule],
 })
 export class PageLayoutModule {}
