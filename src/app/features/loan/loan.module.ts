f5d75166901d076a84e6bc7eb579921b08bfa581
Manitra Randriamanantena <randiluc@gmail.com>
2022-04-29 11:02:47 +0300

    formulaire dans loan-create.component

--- src/app/features/loan/loan.module.ts
+++ src/app/features/loan/loan.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoanCreateComponent } from './loan-create/loan-create.component';
 import { LoanListComponent } from './loan-list/loan-list.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoanCreateComponent, LoanListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule,FormsModule],
   exports: [
     LoanCreateComponent,
     LoanListComponent
