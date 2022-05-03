5cde8cc5098802d36aa16d6fa54088a69c8b2e7f
acf-patrick <miharisoap@gmail.com>
2022-05-03 21:36:53 +0300

    merge dev from loan-create

--- src/app/features/loan/loan-create/loan-create.component.ts
+++ src/app/features/loan/loan-create/loan-create.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-loan-create',
   templateUrl: './loan-create.component.html',
-  styleUrls: ['./loan-create.component.scss']
+  styleUrls: ['./loan-create.component.scss'],
 })
 export class LoanCreateComponent implements OnInit {
+  public date: Date = new Date();
+  formgroup: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formulaire: FormBuilder) {
+    this.formgroup = this.formulaire.group({
+      money: [5000, Validators.required],
+      pourcentage: [0, Validators.required],
+      dateDePret: [this.date.toLocaleDateString(), Validators.required],
+      dateDeRenboursement: [null, Validators.required],
+      ModeDeRemboursement: [null, Validators.required],
+      ModeDePayemment: [null, Validators.required],
+      Commentaire: [null, Validators.required],
+    });
   }
 
+  get formControl(): any {
+    return this.formgroup.controls;
+  }
+
+  ngOnInit(): void {}
+
+  handleSubmit(formgroup: any) {
+    console.log(formgroup);
+  }
 }
--- src/app/features/loan/loan.module.ts
+++ src/app/features/loan/loan.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoanCreateComponent } from './loan-create/loan-create.component';
 import { LoanListComponent } from './loan-list/loan-list.component';
 import { SharedModule } from 'src/app/@shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoanCreateComponent, LoanListComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
   exports: [LoanCreateComponent, LoanListComponent],
 })
 export class LoanModule {}
